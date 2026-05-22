0a90a6389ee25d7c83d638bd2e269d9ccbdfaacd
swati <ranjanswati100@gmail.com>
2026-05-22 13:14:13 +0530

    Initial commit

---
+++ Chapter_05/05_operators.js
@@ -0,0 +1,34 @@
+//assignment operator
+let x=10;
+x=11;
+x=90;
+console.log(x);
+
+//arithmetic operator
+let a=10;
+let b=20;
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+
+//exponential operator
+console.log(10**3);
+
+//compund assignment operator
+let c=10;
+c+=5; //c=c+5
+console.log(c);
+c-=3; //c=c-3
+console.log(c);
+c*=2; //c=c*2
+console.log(c);
+c/=4; //c=c/4
+console.log(c);
+//transitive assignment operator
+let d=10;
+let e=d; //e=d
+console.log(e);     
+"" == "0"; //false 
+"0"==""; //true

48130aa1f21fc20eefe140a9e144baedda76927d
swati <ranjanswati100@gmail.com>
2026-05-24 11:11:26 +0530

    chapter 2 content added

---
+++ Chapter_02_javascript_basics/05_basics.js
@@ -0,0 +1,13 @@
+var $=10;
+var _ =20;
+console.log($);
+console.log(_);
+var NameInput="Swati";
+//camel case
+var fisrtName="Swati";
+//snake case
+var last_name="Ranjan";
+//camelcase
+console.log(NameInput);
+console.log(fisrtName);
+console.log(last_name);
